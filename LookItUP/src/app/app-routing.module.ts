99942b200c1c781e05b798d2fd98a6f2ec4bd6b4
Janelle Unabia <janelleunabia@gmail.com>
2019-04-28 18:45:37 +0800

    added routing

--- LookItUP/src/app/app-routing.module.ts
+++ LookItUP/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'main', component: MainComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// for an easier time in adding routes. so we just have to add in this function, no more additional steps 
+export const routingComponents = [
+  HomeComponent,
+  MainComponent,
+  LoginComponent,
+  RegisterComponent,
+  AboutComponent,
+  ContactComponent
+]
--- LookItUP/src/app/app.module.ts
+++ LookItUP/src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { MainComponent } from './main/main.component';
-import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    ContactComponent,
-    HomeComponent,
-    LoginComponent,
-    MainComponent,
-    RegisterComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
