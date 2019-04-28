e4aede6ecf1f2e4aa7fe7904eb5c18bff1dc6120
Janelle Unabia <janelleunabia@gmail.com>
2019-04-28 08:58:51 +0800

    added components

--- LookItUP/src/app/app.module.ts
+++ LookItUP/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    LoginComponent,
+    MainComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
