b40fa219ebd1b5db2097cdc53f7c1e27e7d19ea8
Janelle Unabia <janelleunabia@gmail.com>
2019-05-20 15:25:21 +0800

    added logout and display currently logged in user's display name

--- LookItUP/src/app/components/main/main.component.ts
+++ LookItUP/src/app/components/main/main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '@app/services/login.service';
+import { ToastrService } from 'ngx-toastr';
+import { UserModel } from '@app/models/user-model';
+import { UserService } from '@app/services/user.service';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  userModel: UserModel = {};
 
-  constructor() { }
+  constructor(private toastr : ToastrService,
+    private loginService: LoginService,
+    private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getCurrentUser().subscribe(
+      documentSnapshot => {
+        this.userModel = documentSnapshot.payload.data();
+      }
+    )
+  }
+
+  signOut(){
+    this.loginService.logoutUser();
+    this.toastr.success(Signed out successfully!')
   }
 
 }
