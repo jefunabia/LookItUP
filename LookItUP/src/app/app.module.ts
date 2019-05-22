3b290bc3d06bae1894ea3de645f733520c736c8a
Janelle Unabia <janelleunabia@gmail.com>
2019-05-22 18:17:33 +0800

    Add files via upload

---
+++ LookItUP/src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { environment } from 'src/environments/environment';
+
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+import { RegisterService } from './services/register.service';
+
+import { LoginComponent } from '@components/login/login.component';
+import { RegisterComponent } from '@components/register/register.component';
+import { AboutComponent } from '@components/about/about.component';
+import { ContactComponent } from '@components/contact/contact.component';
+import { HomeComponent } from '@components/home/home.component';
+import { MainComponent } from '@components/main/main.component';
+
+import { PasswordSecurityService } from './services/password-security.service';
+import { AuthGuardService } from './services/auth/auth-guard.service';
+import { UserService } from './services/user.service';
+import { TokenService } from './services/token.service';
+
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents,
+    RegisterComponent,
+    LoginComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'testtttttt'
+    })
+  ],
+  providers: [RegisterService, PasswordSecurityService, AuthGuardService, UserService, TokenService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
