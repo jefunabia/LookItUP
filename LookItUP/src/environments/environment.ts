5ece3f256eac41f55b144899f42a82297cbc1a7a
Janelle Unabia <janelleunabia@gmail.com>
2019-05-22 18:17:05 +0800

    Add files via upload

---
+++ LookItUP/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDIgy97HAZQHLeKBYMtD-tYWjp-IwZm2yk",
+    authDomain: "look-it-up-d44f0.firebaseapp.com",
+    databaseURL: "https://look-it-up-d44f0.firebaseio.com",
+    projectId: "look-it-up-d44f0",
+    storageBucket: "look-it-up-d44f0.appspot.com",
+    messagingSenderId: "153286798827",
+    appId: "1:153286798827:web:758555f8595d8b7e"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
